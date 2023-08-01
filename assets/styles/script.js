9b657f28c77f0c868241255a9fb785c6b7326af7
unknown <emmansairun@gmail.com>
2023-08-01 14:45:02 +0100

    linter edit

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -1,24 +1,24 @@
-const input = document.querySelector('.input');
-const btn = document.querySelector('.btn');
-const div1 = document.querySelector('.rec1');
-const div2 = document.querySelector('.encoded');
-const div3 = document.querySelector('.rec2');
+const input = document.querySelector('.input')
+const btn = document.querySelector('.btn')
+const div1 = document.querySelector('.rec1')
+const div2 = document.querySelector('.encoded')
+const div3 = document.querySelector('.rec2')
 
 btn.addEventListener('click', function () {
-  secretMessage();
-});
+  secretMessage()
+})
 
 function secretMessage() {
-  const str = input.value;
-  const normalizedInput = str.replace(/[^\w]/g, '').toLowerCase();
-  const strLength = normalizedInput.length;
-  const cols = Math.ceil(Math.sqrt(strLength));
+  const str = input.value
+  const normalizedInput = str.replace(/[^\w]/g, '').toLowerCase()
+  const strLength = normalizedInput.length
+  const cols = Math.ceil(Math.sqrt(strLength))
 
-  let encodeMessage = '';
-  let encode = '';
+  let encodeMessage = ''
+  let encode = ''
 
   if (strLength < 10) {
-    alert('should input at least 10 characters');
+    alert('should input at least 10 characters')
   } else {
     for (let i = 0; i < cols; i++) {
       for (let j = i; j < strLength; j += cols) {
