1a533e2c3d4dc8a3afa70b9dc6f75ab212cd6a84
unknown <emmansairun@gmail.com>
2023-08-01 14:02:24 +0100

    linter edit

--- assets/styles/script.js
+++ assets/styles/script.js
@@ -1,36 +1,36 @@
-const input = document.querySelector(".input")
-const btn = document.querySelector(".btn")
-const div1 = document.querySelector(".rec1")
-const div2 = document.querySelector(".encoded")
-const div3 = document.querySelector(".rec2") 
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+const div1 = document.querySelector('.rec1');
+const div2 = document.querySelector('.encoded');
+const div3 = document.querySelector('.rec2');
 
 btn.addEventListener('click', function () {
-  secretMessage()
-})
+  secretMessage();
+});
 
-function secretMessage () {
-  let str = input.value
-  const normalizedInput = str.replace(/[^\w]/g, "").toLowerCase();
+function secretMessage() {
+  const str = input.value;
+  const normalizedInput = str.replace(/[^\w]/g, '').toLowerCase();
   const strLength = normalizedInput.length;
   const cols = Math.ceil(Math.sqrt(strLength));
 
-  var encodeMessage = "";
-  var encode = "";
+  let encodeMessage = '';
+  let encode = '';
 
   if (strLength < 10) {
-    alert("should input at least 10 characters")
+    alert('should input at least 10 characters');
   } else {
-    for (let i = 0; i < cols; i ++) { 
+    for (let i = 0; i < cols; i++) {
       for (let j = i; j < strLength; j += cols) {
         encodeMessage += normalizedInput[j];
         encode += normalizedInput[j];
       }
-      encodeMessage += '\n'
+      encodeMessage += '\n';
     }
-    
-    div1.innerHTML = `${normalizedInput}`
-    div2.innerHTML = `${encodeMessage} `
-    div3.innerHTML = `${encode}`
+
+    div1.innerHTML = `${normalizedInput}`;
+    div2.innerHTML = `${encodeMessage} `;
+    div3.innerHTML = `${encode}`;
   }
-console.log(normalizedInput);
+  console.log(normalizedInput);
 }
